cce461708af92aae1abea251f276a89d6c549a00
Viana <vbp13@miami.edu>
2019-10-21 15:00:10 -0400

    js

---
+++ class_examples/js_intro/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+var x = 2 + 2;
+console.log(x);
+// var x = 2 + "2" like this puts two string together, so it would equal 22 on the page/instead of doing maths it reads;
+  // console.log("Everything loaded!");
+  // console.log("this function ran");
+});
+
+
+
+// var array1 = [1,2,3,4,5,6];
+// var array2 = ["This is a string", 'This is also a string'];
+// var x = 4;
+// var heading = document.getElementsByTagName("h1");
+// console.log(heading);
+// console.log(x);
+
+// use jquery, download or use online, to check codess;
+// $ means a jquery;
